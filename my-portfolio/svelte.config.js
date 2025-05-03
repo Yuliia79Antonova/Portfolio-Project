e298c70062a25afef84406c34ebdda2e3be1bba8
Yuliia Antonova <C00301432@setu.ie>
2025-05-03 20:07:11 +0100

    For deploy2

--- my-portfolio/svelte.config.js
+++ my-portfolio/svelte.config.js
@@ -1,15 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	
-	preprocess: vitePreprocess(),
-
-	kit: {
-		
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
